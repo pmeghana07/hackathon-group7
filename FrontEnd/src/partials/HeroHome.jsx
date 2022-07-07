46681de15fe0197183cee8d907a31f2693ae93e7
xuyisuzy <e0424897@u.nus.edu>
2022-07-07 17:07:59 +0800

    calendar view added

--- FrontEnd/src/partials/HeroHome.jsx
+++ FrontEnd/src/partials/HeroHome.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../utils/Modal';
+import { Link } from 'react-router-dom';
 
 import HeroImage from '../images/hero-image.png';
 import OutDoorImage from "../images/outdoor.png";
@@ -40,10 +41,7 @@ function HeroHome() {
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Let's Meet is a platform for you to meet new people and do activities together! Simply create a new event or join events organised by your fellow colleagues!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Post new events</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Explore events</a>
+                    <Link to="calendar" className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0">Explore events</Link>
                 </div>
               </div>
             </div>
