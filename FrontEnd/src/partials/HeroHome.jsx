0cf32d8209d8f92d66cc75c8ccba28cc40cf55ab
wjh <2261696400wjh@gmail.com>
2022-07-07 17:26:11 +0800

    Link the post event link to the button

--- FrontEnd/src/partials/HeroHome.jsx
+++ FrontEnd/src/partials/HeroHome.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../utils/Modal';
+import { Link } from 'react-router-dom';
 
 import HeroImage from '../images/hero-image.png';
 import OutDoorImage from "../images/outdoor.png";
@@ -39,12 +40,13 @@ function HeroHome() {
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Let's Meet is a platform for you to meet new people and do activities together! Simply create a new event or join events organised by your fellow colleagues!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Explore events</a>
+                <div className = "max-w-6xl mx-auto px-4 sm:px-6">
+                    <Link to="calendar" className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0">Explore events</Link>
                 </div>
-                {/* <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Explore events</a>
-                </div> */}
+
+                <div className = "max-w-6xl mx-auto px-4 sm:px-6">
+                    <Link to="signup" className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0">Post new events</Link>
+                </div>
               </div>
             </div>
           </div>
