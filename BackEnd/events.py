66fafee496c9c838b8f54ebaac07eb8f2c21f337
rafeang <rafe.ang.2019@sis.smu.edu.sg>
2022-07-08 11:41:03 +0800

    events update

--- BackEnd/events.py
+++ BackEnd/events.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import boto3
-import json
+import simplejson as json
 import uuid
 
 client = boto3.client('dynamodb')
@@ -9,15 +9,21 @@ print('Loading event service...')
 
 def handler(event, context):
 
-  path = event['requestContext']["path"]
+  path = event['requestContext']["http"]["path"]
   requestJson = json.loads(event['body'])
+  TABLE_NAME = "eventTable"
+  dynamodb = boto3.resource('dynamodb', region_name="ap-southeast-1")
+  table = dynamodb.Table(TABLE_NAME)
 
   def add_event():
+    category_list = []
+    for category in requestJson["categories"]:
+      category_list.append({"S": category})
     dynamo_response = client.put_item(
-      TableName='eventTable',
+      TableName=TABLE_NAME,
       Item={
         'eventId': {
-          'S': uuid.uuid4()
+          'S': str(uuid.uuid4())
         },
         'status': {
           'S': 'OPEN'
@@ -37,19 +43,22 @@ def handler(event, context):
         'date': {
           'S': requestJson["date"]
         },
-        'tags': {
-          'L': requestJson["tags"]
+        'categories': {
+          'L': category_list
         },
-        'name': {
-          'S': requestJson["name"]
+        'event_name': {
+          'S': requestJson["event_name"]
+        },
+        'event_description': {
+          'S': requestJson["event_description"]
         },
         'sizeCap': {
           'N': requestJson["sizeCap"]
         },
-        'contactPerson': {
+        'contact_person': {
           'M': {
-            "telegram": {
-              'S': requestJson["contactPersonTelegram"]
+            "email": {
+              'S': requestJson["contactPersonEmail"]
             },
             "kerberosId": {
               "S": requestJson["contactPersonKerberos"]
@@ -61,28 +70,74 @@ def handler(event, context):
         },
       }
     )
-    return dynamo_response
+    return dynamo_response["ResponseMetadata"]["HTTPStatusCode"]
     
+  def add_participant():  
+    dynamo_response = client.update_item(
+      TableName=TABLE_NAME,
+      Key = {
+        "eventId": {
+            "S": requestJson["eventId"]
+        },
+      },
+      UpdateExpression="SET participants = list_append(participants, :new_participant)",
+      ExpressionAttributeValues={
+        ':new_participant': {
+          "L": [
+            {
+              "M":{
+                "name":{
+                  "S":requestJson["name"]
+                },
+                "email":{
+                  "S":requestJson["email"]
+                },
+                "kerberos":{
+                  "S":requestJson["kerberos"]
+                }
+              }
+            },
+          ]
+        }
+      },
+      ReturnValues="UPDATED_NEW"
+    )
+    return dynamo_response["ResponseMetadata"]["HTTPStatusCode"]
+
   # def edit_event():
 
   # def delete_event():
 
-  # def get_all_events():
+  def get_all_events():
+    dynamo_response = table.scan()
+    print("GET_ALL")
+    print(dynamo_response)
+    return dynamo_response["Items"]
 
-  # def get_event_by_id():
+  def get_event_by_id():
+    dynamo_response = table.get_item(
+      Key={
+        "eventId": requestJson["eventId"]
+      }
+    )
+    print("GET_ITEM")
+    print(dynamo_response)
+    return dynamo_response["Item"]
 
   # def get_filtered_events():
+    # date, category, OPEN/CLOSE (status), location, sizeCap
 
   execute = {
-    '/events/add': add_event
+    '/events/add': add_event,
     # '/events/edit': edit_event,
     # '/events/delete': delete_event,
-    # '/events/all': get_all_events,
-    # '/events/id': get_event_by_id,
+    '/events/all': get_all_events,
+    '/events/id': get_event_by_id,
     # '/events/filter': get_filtered_events,
+    '/events/addParticipant': add_participant
   }
 
-  data = execute[path]
+  data = execute[path]()
 
   response = {
       'statusCode': 200,
@@ -92,5 +147,5 @@ def handler(event, context):
         'Access-Control-Allow-Origin': '*'
       },
   }
-  
+
   return response
